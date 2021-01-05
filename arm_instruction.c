c321ededb9b94dd34ac600921c1b74a7f8001794
Tardy Jimmy <tardyjim26@gmail.com>
2021-01-05 12:34:28 +0100

    instruction

--- arm_instruction.c
+++ arm_instruction.c
@@ -1,24 +1,24 @@
 /*
-Armator - simulateur de jeu d'instruction ARMv5T à but pédagogique
+Armator - simulateur de jeu d'instruction ARMv5T ï¿½ but pï¿½dagogique
 Copyright (C) 2011 Guillaume Huard
 Ce programme est libre, vous pouvez le redistribuer et/ou le modifier selon les
-termes de la Licence Publique Générale GNU publiée par la Free Software
-Foundation (version 2 ou bien toute autre version ultérieure choisie par vous).
+termes de la Licence Publique Gï¿½nï¿½rale GNU publiï¿½e par la Free Software
+Foundation (version 2 ou bien toute autre version ultï¿½rieure choisie par vous).
 
-Ce programme est distribué car potentiellement utile, mais SANS AUCUNE
+Ce programme est distribuï¿½ car potentiellement utile, mais SANS AUCUNE
 GARANTIE, ni explicite ni implicite, y compris les garanties de
-commercialisation ou d'adaptation dans un but spécifique. Reportez-vous à la
-Licence Publique Générale GNU pour plus de détails.
+commercialisation ou d'adaptation dans un but spï¿½cifique. Reportez-vous ï¿½ la
+Licence Publique Gï¿½nï¿½rale GNU pour plus de dï¿½tails.
 
-Vous devez avoir reçu une copie de la Licence Publique Générale GNU en même
-temps que ce programme ; si ce n'est pas le cas, écrivez à la Free Software
+Vous devez avoir reï¿½u une copie de la Licence Publique Gï¿½nï¿½rale GNU en mï¿½me
+temps que ce programme ; si ce n'est pas le cas, ï¿½crivez ï¿½ la Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307,
-États-Unis.
+ï¿½tats-Unis.
 
 Contact: Guillaume.Huard@imag.fr
-	 Bâtiment IMAG
+	 Bï¿½timent IMAG
 	 700 avenue centrale, domaine universitaire
-	 38401 Saint Martin d'Hères
+	 38401 Saint Martin d'Hï¿½res
 */
 #include "arm_instruction.h"
 #include "arm_exception.h"
@@ -29,7 +29,140 @@ Contact: Guillaume.Huard@imag.fr
 #include "util.h"
 
 static int arm_execute_instruction(arm_core p) {
-    return 0;
+    uint32_t instruction;
+    uint8_t error = 0;
+    //Verification fetch memory
+    if (arm_fetch(p, &instruction)) {
+        printf("Error: Fetch memory");
+        error = 1;
+    } else {
+        //Verification flags
+        uint32_t val_cpsr = arm_read_cpsr(p);
+        int V_flag = (val_cpsr >> V) & 1;
+        int C_flag = (val_cpsr >> C) & 1;
+        int Z_flag = (val_cpsr >> Z) & 1;
+        int N_flag = (val_cpsr >> N) & 1;
+        switch (instruction >> 28) {
+        case 0b0000: //EQ
+            if (!Z_flag) {
+                error = 1;
+            } 
+            break;
+        case 0b0001: //NE
+            if (Z_flag) {
+                error = 1;
+            } 
+            break;
+        case 0b0010: //CS/HS
+            if (!C_flag) {
+                error = 1;
+            } 
+            break;
+        case 0b0011: //CC/LO
+            if (C_flag) {
+                error = 1;
+            } 
+            break;
+        case 0b0100: //MI
+            if (!N_flag) {
+                error = 1;
+            } 
+            break;
+        case 0b0101: //PL
+            if (N_flag) {
+                error = 1;
+            } 
+            break;
+        case 0b0110: //VS
+            if (!V_flag) {
+                error = 1;
+            } 
+            break;
+        case 0b0111: //VC
+            if (V_flag) {
+                error = 1;
+            } 
+            break;
+        case 0b1000: //HI
+            if (!C_flag || Z_flag) {
+                error = 1;
+            } 
+            break;
+        case 0b1001: //LS
+            if (!(!C_flag || Z_flag)) {
+                error = 1;
+            }
+            break;
+        case 0b1010: //GE
+            if (N_flag != V_flag) {
+                error = 1;
+            }
+            break;
+        case 0b1011: //LT
+            if (N_flag == V_flag) {
+                error = 1;
+            }
+            break;
+        case 0b1100: //GT
+            if (Z_flag || N_flag != V_flag) {
+                error = 1;
+            }
+            break;
+        case 0b1101: //LE
+            if (!(Z_flag || N_flag != V_flag)) {
+                error = 1;
+            }
+            break;      
+        default:
+            break;
+        }
+    }
+    //Distribution de l'instruction Ã  la bonne fonction
+    if (!error) {
+        uint8_t typeop = get_bits(instruction, 27, 25);
+        switch (typeop) {
+            case 0:
+                if (get_bit(instruction, 4) == 0 || get_bit(instruction, 7) == 0) {
+                    // Data processing immediate shift or register shift
+                    arm_data_processing_shift(p, instruction);
+                } else if (get_bit(instruction, 7) == 1 && get_bit(instruction, 4) == 1) {
+                    //Extra load/stores
+                    arm_load_store(p, instruction);
+                }  else {
+                    arm_miscellaneous(p, instruction);
+                }
+                break;
+            case 1:
+                //MSR: Immediate operand
+                arm_data_processing_immediate_msr(p, instruction);
+            break;
+            case 2: 
+                //Load and Store: Immediate offset/index
+                arm_load_store(p, instruction);
+                break;
+            case 3 :
+                //Load and Store: Register offset/index
+                arm_load_store(p, instruction);
+                // Media instructions and Architecturally undefined
+                break;
+            default:
+            case 4:
+                arm_load_store_multiple(p, instruction);
+            break;
+            case 5:
+                //BLX (1)
+                arm_branch(p, instruction);
+            break;
+            case 6:
+                //Load and Store Coprocessor
+                arm_coprocessor_load_store(p, instruction);
+            case 7:
+                //Exception-generating instructions
+                arm_coprocessor_others_swi(p, instruction);
+            break;
+        }
+    }
+    return error;
 }
 
 int arm_step(arm_core p) {
