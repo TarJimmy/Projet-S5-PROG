6c2b56951f63718f2fe72ef8dbc3521795f9e308
Tardy Jimmy <tardyjim26@gmail.com>
2021-01-07 11:13:43 +0100

    load_store byte-word-halfword-doubleword

--- arm_load_store.h
+++ arm_load_store.h
@@ -1,24 +1,24 @@
 /*
-Armator - simulateur de jeu d'instruction ARMv5T à but pédagogique
+Armator - simulateur de jeu d'instruction ARMv5T ï¿½ but pï¿½dagogique
 Copyright (C) 2011 Guillaume Huard
 Ce programme est libre, vous pouvez le redistribuer et/ou le modifier selon les
-termes de la Licence Publique Générale GNU publiée par la Free Software
-Foundation (version 2 ou bien toute autre version ultérieure choisie par vous).
+termes de la Licence Publique Gï¿½nï¿½rale GNU publiï¿½e par la Free Software
+Foundation (version 2 ou bien toute autre version ultï¿½rieure choisie par vous).
 
-Ce programme est distribué car potentiellement utile, mais SANS AUCUNE
+Ce programme est distribuï¿½ car potentiellement utile, mais SANS AUCUNE
 GARANTIE, ni explicite ni implicite, y compris les garanties de
-commercialisation ou d'adaptation dans un but spécifique. Reportez-vous à la
-Licence Publique Générale GNU pour plus de détails.
+commercialisation ou d'adaptation dans un but spï¿½cifique. Reportez-vous ï¿½ la
+Licence Publique Gï¿½nï¿½rale GNU pour plus de dï¿½tails.
 
-Vous devez avoir reçu une copie de la Licence Publique Générale GNU en même
-temps que ce programme ; si ce n'est pas le cas, écrivez à la Free Software
+Vous devez avoir reï¿½u une copie de la Licence Publique Gï¿½nï¿½rale GNU en mï¿½me
+temps que ce programme ; si ce n'est pas le cas, ï¿½crivez ï¿½ la Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307,
-États-Unis.
+ï¿½tats-Unis.
 
 Contact: Guillaume.Huard@imag.fr
-	 Bâtiment IMAG
+	 Bï¿½timent IMAG
 	 700 avenue centrale, domaine universitaire
-	 38401 Saint Martin d'Hères
+	 38401 Saint Martin d'Hï¿½res
 */
 #ifndef __ARM_LOAD_STORE_H__
 #define __ARM_LOAD_STORE_H__
@@ -29,4 +29,20 @@ int arm_load_store(arm_core p, uint32_t ins);
 int arm_load_store_multiple(arm_core p, uint32_t ins);
 int arm_coprocessor_load_store(arm_core p, uint32_t ins);
 
+//Function Utile
+
+//Calculate adress associate at U_bit
+uint32_t handling_offset(uint32_t address,int offset,uint8_t U_bit);
+
+//Call function associate at W_bit
+//(W_bit == 0) normal memory acces
+//(W_bit == 1) unprivileged (User mode) memory access 
+void write_register_mode(arm_core p, uint8_t reg, uint32_t value, uint8_t W_bit);
+uint32_t read_register_mode(arm_core p, uint8_t reg, uint8_t W_bit);
+
+int word_byte_load_store(arm_core p, uint8_t I_bit, uint8_t P_bit, uint8_t U_bit, uint8_t B_bit, uint8_t W_bit, uint8_t L_bit, uint8_t rn, uint8_t rd, uint16_t data, uint8_t cond);
+
+int miscellaneous_load_store(arm_core p, uint8_t I_bit, uint8_t P_bit, uint8_t U_bit, uint8_t B_bit, uint8_t W_bit, uint8_t L_bit, uint8_t rn, uint8_t rd, uint16_t data, uint8_t cond);
+
+int load_store_half_double_byte (arm_core p, uint8_t rd, uint32_t address, uint8_t L_bit, uint8_t S_bit, uint8_t H_bit, uint8_t cond);
 #endif
