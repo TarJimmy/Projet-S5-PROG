e71780a757aec8d20e9ef60d21ba7a91f720186f
Laëtitia <61683295+Laeti-Avo@users.noreply.github.com>
2021-01-13 18:32:08 +0100

    factorisation shift

--- decode.c
+++ decode.c
@@ -15,110 +15,78 @@ void update_shifter_carry_out(uint32_t* shifter_carry_out, uint32_t value) {
 		*shifter_carry_out = value;
 	}
 }
+
 uint32_t shift(arm_core p, uint32_t instruction, uint32_t* shifter_carry_out){
 	uint32_t shift = get_bits(instruction, 6, 5);
     uint32_t offset = arm_read_register(p, get_bits(instruction, 3, 0)); //shifter_operand = Rm
     uint32_t bit_4 = get_bit(instruction, 4); //(shift) 0=valeur_immédiate 1=registre
-    uint32_t valeur = 0;
-    if (bit_4) {  
-    	uint8_t Rs = get_bits(instruction, 11, 8);
-    	valeur = arm_read_register(p, Rs);
-    	uint32_t Rs_7_0 = get_bits(valeur,7,0);
-    	uint32_t Rs_4_0 = get_bits(valeur,4,0);
-		if (Rs_7_0 == 0 && shift >= 0 && shift <= 3) {
-			update_shifter_carry_out(shifter_carry_out, (arm_read_cpsr(p) >> C) & 1);
-		} else if (shift < 2 && Rs_7_0 > 32) {
-			offset = 0;
-			update_shifter_carry_out(shifter_carry_out, 0);
-		} else {
-			switch (shift) {
-				case 0: //LSL
-					if (Rs_7_0 < 32) {
-						offset = offset << Rs_7_0;
-						update_shifter_carry_out(shifter_carry_out, get_bit(arm_read_register(p, offset), (32 - Rs_7_0)));
-					} else if (Rs_7_0 == 32) {
+    uint8_t Rs = get_bits(instruction, 11, 8);
+	uint32_t val_reg = arm_read_register(p, Rs);
+	uint32_t Rs_7_0 = get_bits(val_reg,7,0);
+	uint32_t Rs_4_0 = get_bits(val_reg,4,0);
+	uint32_t val_imm = get_bits(instruction, 11, 7);
+	uint32_t valeur = 0;
+    if (bit_4 && Rs_7_0 == 0 && shift >= 0 && shift <= 3) {
+		update_shifter_carry_out(shifter_carry_out, (arm_read_cpsr(p) >> C) & 1);
+	} else if (bit_4 && shift < 2 && Rs_7_0 > 32) {
+		offset = 0;
+		update_shifter_carry_out(shifter_carry_out, 0);
+	} else {
+		switch (shift){
+			case 0: //LSL
+				if ((Rs_7_0 < 32) || (val_imm!=0)) {
+					if (bit_4) {valeur = Rs_7_0;} else {valeur = val_imm;}
+					offset = offset << valeur;
+					update_shifter_carry_out(shifter_carry_out, get_bit(arm_read_register(p, offset), (32 - valeur)));
+				} else if (bit_4 && Rs_7_0 == 32) {
+					offset = 0;
+					update_shifter_carry_out(shifter_carry_out, get_bit(arm_read_register(p, offset), 0));
+				} else if (!bit_4) {
+					update_shifter_carry_out(shifter_carry_out, (arm_read_cpsr(p) >> C) & 1);
+				}
+				break;
+			case 1: //LSR
+				if ((Rs_7_0 < 32) || (val_imm!=0)) {
+					if (bit_4) {valeur = Rs_7_0;} else {valeur = val_imm;}
+					offset = offset >> valeur;
+					update_shifter_carry_out(shifter_carry_out, get_bit(arm_read_register(p, offset), (valeur - 1)));
+				} else if ((bit_4 && Rs_7_0 == 32) || (!bit_4 && val_imm == 0)) {
+					offset = 0;
+					update_shifter_carry_out(shifter_carry_out, get_bit(arm_read_register(p, offset), 31));
+				}
+				break;
+			case 2: //ASR
+				if (bit_4 && Rs_7_0 < 32) {
+					offset = asr(offset, Rs_7_0);
+					update_shifter_carry_out(shifter_carry_out, get_bit(arm_read_register(p, offset), Rs_7_0-1));
+				} else if ((bit_4 && Rs_7_0 >= 32) || (!bit_4 && val_imm == 0)) {
+					if (!get_bit(arm_read_register(p, offset), 31)) {
 						offset = 0;
-						update_shifter_carry_out(shifter_carry_out, get_bit(arm_read_register(p, offset), 0));
-					} 
-					break;
-				case 1: //LSR
-					if (Rs_7_0 < 32) {
-						offset = offset >> Rs_7_0;
-						update_shifter_carry_out(shifter_carry_out, get_bit(arm_read_register(p, offset), (Rs_7_0 - 1)));
-					} else if (Rs_7_0 == 32) {
-						offset = 0;
-						update_shifter_carry_out(shifter_carry_out, get_bit(arm_read_register(p, offset), 31));
-					}
-					break;
-				case 2: //ASR
-					if (Rs_7_0 < 32) {
-						offset = asr(offset, Rs_7_0);
-						update_shifter_carry_out(shifter_carry_out, get_bit(offset, Rs_7_0-1));
 					} else {
-						if (!get_bit(arm_read_register(p, offset), 31)) {
-							offset = 0;
-						} else {
-							offset = 0xFFFFFFFF;
-						}
-						update_shifter_carry_out(shifter_carry_out, get_bit(arm_read_register(p, offset),31));
+						offset = 0xFFFFFFFF;
 					}
-					break;
-				case 3: //ROR
-					if (Rs_4_0==0) {
-						update_shifter_carry_out(shifter_carry_out, get_bit(arm_read_register(p, offset),31));
-					} else {
-						offset = ror(offset, Rs_4_0);
-						update_shifter_carry_out(shifter_carry_out, get_bit(arm_read_register(p, offset),Rs_4_0-1));
-					}
-					break;
-				default:
-					break;
-			}
-		}
-    } else {
-    	valeur = get_bits(instruction, 11, 7);
-    	switch (shift) {
-		    case 0: //LSL
-				update_shifter_carry_out(shifter_carry_out, (arm_read_cpsr(p) >> C) & 1);
-		        if (valeur!=0) {
-		        	offset = offset << valeur;
-					update_shifter_carry_out(shifter_carry_out, get_bit(arm_read_register(p, offset),32-valeur));
-		        }
-		        break;
-		    case 1: //LSR
-		        if (valeur == 0) {
-		            offset = 0;
 					update_shifter_carry_out(shifter_carry_out, get_bit(arm_read_register(p, offset),31));
-		        } else {
-		            offset = offset >> valeur;
+				} else if (!bit_4 && val_imm != 0) {
+					offset = asr(offset, val_imm);
+					update_shifter_carry_out(shifter_carry_out, get_bit(arm_read_register(p, offset),val_imm-1));
+				}
+				break;
+			case 3: //ROR
+				if (Rs_4_0>0 || val_imm !=0) {
+					if (bit_4) {valeur = Rs_7_0;} else {valeur = val_imm;}
+					offset = ror(offset, valeur);
 					update_shifter_carry_out(shifter_carry_out, get_bit(arm_read_register(p, offset),valeur-1));
-		        }
-		        break;
-		    case 2: //ASR
-		        if (valeur == 0) {
-		            if (!get_bit(arm_read_register(p, offset),31)) {
-		            	offset = 0;
-		            } else {
-		            	offset = 0xFFFFFFFF;
-		            }
+				} else if (bit_4 && Rs_4_0==0) {
 					update_shifter_carry_out(shifter_carry_out, get_bit(arm_read_register(p, offset),31));
-		        } else {
-		            offset = asr(offset, valeur);
-					update_shifter_carry_out(shifter_carry_out, get_bit(arm_read_register(p, offset),valeur-1));
-		        }
-		        break;
-		    case 3: //ROR
-		        if (valeur ==0) {
-		            offset = (((arm_read_cpsr(p) >> C) & 1) << 31) | (offset >> 1) ;
+				} else if (!bit_4 && val_imm==0) {
+					offset = (((arm_read_cpsr(p) >> C) & 1) << 31) | (offset >> 1) ;
 					update_shifter_carry_out(shifter_carry_out, get_bit(arm_read_register(p, offset),0));
-		        } else {
-		            offset = ror(offset, valeur);
-					update_shifter_carry_out(shifter_carry_out, get_bit(arm_read_register(p, offset),valeur-1));
-		        }
-		    default:
-		        break;
-    	} 
-    }
+				}
+				break;
+			default:
+				break;
+		}
+	}
     return offset;
 }
 
