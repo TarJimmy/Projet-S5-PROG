148a4b188606423fd1598cd76b4ab3ccbd76b5e0
telmoms <telmosantosbp@hotmail.com>
2021-01-13 11:52:17 +0100

    MRS

--- Examples/example2.s
+++ Examples/example2.s
@@ -1,7 +1,11 @@
 .global main
 .text
 main:
-    ldr r1, =donnee
+    ldr r2, =donnee
+    ldmia r2, {r5 - r6}
+    ldmib r2, {r7 - r8}
+    ldmda r2, {r3 - r4}
+    ldmdb r2, {r1,r9}
     swi 0x123456
 .data
 donnee:
